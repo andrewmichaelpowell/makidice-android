5c6522095323c73ecbd9ba02a3af547a9d7b1eaf
Andrew Powell <andrewmichaelpowell@icloud.com>
2026-07-11 14:29:38 +0200

    Version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "xyz.andrewmichaelpowell.makidice"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.7"
     }
 
